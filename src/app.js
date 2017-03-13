6cf48755c8adecd5f64c24669f9b99ea2ea244d7
Rudraksh MK <rudraksh@paperkite.in>
2017-03-13 11:07:49 +0530

    Added an endpoint that uses an ES7 async function.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("babel-core/register")
 require("babel-polyfill")
 
 const server = require('diet')
+const got = require('got')
 let app = server()
 app.listen('http://localhost:8000')
 
@@ -9,4 +10,11 @@ let HelloWorld = function($){
     $.end('Hello World!')
 }
 
+async function getGithubDetails($){
+    let response = await got('https://api.github.com/users/rudimk')
+    let user = JSON.parse(response.body)
+    $.json(user)
+}
+
 app.get('/hello', HelloWorld)
+app.get('/github', getGithubDetails)
